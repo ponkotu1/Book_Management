6280380313ab48773b1318b4b9f57f02a322f0da
takumi <t.souma.sys22@morijyobi.ac.jp>
2023-03-24 09:52:17 +0900

    .gitignoreno

---
+++ src/main/java/dto/Book.java
@@ -0,0 +1,78 @@
+package dto;
+
+public class Book{
+    private int id;
+    private String title;
+    private String author;
+    private String publisher;
+    private String isbn;
+    private String category;
+    private int rent;
+
+    public Book(int id, String title, String author, String publisher, String isbn, String category, int rent) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.category = category;
+        this.rent = rent;
+    }
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public void setRent(int rent) {
+        this.rent = rent;
+    }
+}
